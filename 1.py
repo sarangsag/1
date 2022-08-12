6753a5b6de6713df2d96d80966eafe916e8604f3
sarangsag <110149457+sarangsag@users.noreply.github.com>
2022-08-12 18:36:44 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+# a=int(input("enter the num"))
+#
+# s=a
+# rev=0
+# while(a>0):
+#     r=a%10
+#     rev=(rev*10)+r
+#     a=int(a/10)
+# print("rev is ",rev)
+n=5
+for i in range(n,0,-1):
+    print()
